f6a2417cabe3064a494fa43f09911168d8e2c0cb
Albertini <albertini.da@gmail.com>
2013-03-19 13:26:46 +0100

    fix date

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<h2>PersDB 2013</h2>
 		<h5>7th International Workshop on</h5>
 		<h3>Personalized Access, Profile Management, and Context Awareness</h3><h3>in Databases</h3>
-		<h4>26<sup>th</sup> - 30<sup>th</sup> August, 2013 - Riva del Garda, Trento, Italy</h4>
+		<h4>30<sup>th</sup> August, 2013 - Riva del Garda, Trento, Italy</h4>
 	</div>
 	<div class="vldbLogo" onClick="window.open('http://www.vldb.org/2013/')"></div>
 </div>
