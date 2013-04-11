33a089324a6dc7b6046a7112484a70de7a06a4b7
Albertini <albertini.da@gmail.com>
2013-04-11 14:06:18 +0200

    organization updated

--- pages/organization.php
+++ pages/organization.php
@@ -5,13 +5,32 @@
 <!--TODO funzione js bindata ai nomi, che prende nome e cognome e manda mail -->
 	
 	<table border="0" cellspacing="5" cellpadding="5">
+		
 		<tr><th>STEERING COMMITTEE</th><th></th></tr>
 		<tr><td class="mailto">Yannis Ioannidis</td><td>(University of Athens, Hellas)</td></tr>
 		<tr><td class="mailto" data-name="koutrika" data-domain="hp.com">Georgia Koutrika</td><td>(HP Labs, USA)</td></tr>
+		
 		<tr><th>PC CHAIRS</th><th></th></tr>
 		<tr><td class="mailto" data-name="barbara.carminati" data-domain="uninsubria.it">Barbara Carminati</td><td>(University of Insubria, Italy)</td></tr>
+		<tr><td>Pankaj Mehra</td><td>(Whodini)</td></tr>
+		
 		<tr><th>PC MEMBERS</th><th></th></tr>
-		<tr><td>TBA</td><td></td></tr>
+		<tr><td>Sylvie Cazalens</td><td>(University of Nantes)</td></tr>	
+		<tr><td>Eelco Herder</td><td>(L3S Research Center)</td></tr>	
+		<tr><td>Mohamed	Mokbel</td><td>(University of Minnesota)</td></tr>	
+		<tr><td>Georgios Paliouras</td><td>(NCSR Demokritos)</td></tr>	
+		<tr><td>Gabriella Pasi</td><td>(Università degli Studi di Milano Bicocca)</td></tr>	
+		<tr><td>Dimitris Pierrakos</td><td>(NCSR Demokritos)</td></tr>	
+		<tr><td>Evaggelia Pitoura</td><td>(University of Ioannina)</td></tr>	
+		<tr><td>Mohamed	Sarwat</td><td>(University Of Minnesota)</td></tr>	
+		<tr><td>Heiko	Schuldt</td><td>(University of Basel)</td></tr>	
+		<tr><td>Andrey	Simanovsky</td><td>(HP Labs Russia)</td></tr>	
+		<tr><td>Kostas	Stefanidis</td><td>(Institute of Computer Science at FORTH)</td></tr>
+		<tr><td>Letizia	Tanca</td><td>(Politecnico di Milano)</td></tr>	
+		<tr><td>Shu Tao</td><td>(IBM T.J.Watson Research Center)</td></tr>	
+		<tr><td>Alexander Ulanov</td><td>(HP Labs Russia)</td></tr>	
+		<tr><td>Min Xie</td><td>(University of British Columbia)</td></tr>		
+		
 		<tr><th>PersDB 2013 WEBSITE</th><th></th></tr>
 		<tr><td class="mailto" data-name="davide.albertini" data-domain="uninsubria.it">Davide Albertini</td><td>(University of Insubria, Italy)</td></tr>
 	</table>
