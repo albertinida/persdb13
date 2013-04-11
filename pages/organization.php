1dcdde055a65fec1d4d95ed9b9c2449297315cdb
Albertini <albertini.da@gmail.com>
2013-04-11 14:26:57 +0200

    organization updated

--- pages/organization.php
+++ pages/organization.php
@@ -1,17 +1,14 @@
 <div class="organization">
 	<h3>ORGANIZATION</h3>
-	<h6>Click on names to contact via email</h6>
-
-<!--TODO funzione js bindata ai nomi, che prende nome e cognome e manda mail -->
 	
 	<table border="0" cellspacing="5" cellpadding="5">
 		
 		<tr><th>STEERING COMMITTEE</th><th></th></tr>
-		<tr><td class="mailto">Yannis Ioannidis</td><td>(University of Athens, Hellas)</td></tr>
-		<tr><td class="mailto" data-name="koutrika" data-domain="hp.com">Georgia Koutrika</td><td>(HP Labs, USA)</td></tr>
+		<tr><td>Yannis Ioannidis</td><td>(University of Athens, Hellas)</td></tr>
+		<tr><td>Georgia Koutrika</td><td>(HP Labs, USA)</td></tr>
 		
 		<tr><th>PC CHAIRS</th><th></th></tr>
-		<tr><td class="mailto" data-name="barbara.carminati" data-domain="uninsubria.it">Barbara Carminati</td><td>(University of Insubria, Italy)</td></tr>
+		<tr><td>Barbara Carminati</td><td>(University of Insubria, Italy)</td></tr>
 		<tr><td>Pankaj Mehra</td><td>(Whodini)</td></tr>
 		
 		<tr><th>PC MEMBERS</th><th></th></tr>
@@ -32,10 +29,7 @@
 		<tr><td>Min Xie</td><td>(University of British Columbia)</td></tr>		
 		
 		<tr><th>PersDB 2013 WEBSITE</th><th></th></tr>
-		<tr><td class="mailto" data-name="davide.albertini" data-domain="uninsubria.it">Davide Albertini</td><td>(University of Insubria, Italy)</td></tr>
+		<tr><td>Davide Albertini</td><td>(University of Insubria, Italy)</td></tr>
 	</table>
 
-	<script>
-		if (typeof $j != 'undefined') bindEmailContacts();
-	</script>
 </div>
