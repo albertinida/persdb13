ed9232196185e10cdc25b00d8789da3fa49ddc22
Albertini <albertini.da@gmail.com>
2013-05-09 15:06:01 +0200

    keynote

--- pages/keynotes.php
+++ pages/keynotes.php
@@ -2,7 +2,7 @@
 	<div class="keynote">
 		<h3>Keynote speaker: Maurice Hendrix</h3> 
 
-		<h5>Serious games meets Adaptive Hypermedia: Integrating games into web-based e-learning systems.</h5>
+		<h4>Serious games meets Adaptive Hypermedia: Integrating games into web-based e-learning systems.</h4>
 
 		<p>Dr Maurice Hendrix is a Senior Researcher at the Serious Games Institute. He holds a PhD in Computer Science from The University of Warwick and an MSc and BSc also in Computer Science from Eindhoven University of Technology, The Netherlands. His research interests are centered on emerging educational technology, intelligent web-based systems, authoring and serious games. He has experience of working in European research projects, expertise in the evaluation of systems, and has also worked in technical IT roles, working with large data sets such as the human genome.</p>
 
