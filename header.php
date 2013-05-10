59f33d0dcb511bf8687935fa487b4e772c6bb430
Albertini <albertini.da@gmail.com>
2013-05-10 12:10:43 +0200

    fix date display

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<div class="persdbLogo"></div>
 	<div class="title">
 		<h2>PersDB 2013</h2>
-		<h5>7th International Workshop on</h5>
+		<h5>7<sup>th</sup> International Workshop on</h5>
 		<h3>Personalized Access, Profile Management, and Context Awareness</h3><h3>in Databases</h3>
 		<h4>30<sup>th</sup> August, 2013 - Riva del Garda, Trento, Italy</h4>
 	</div>
--- pages/dates.php
+++ pages/dates.php
@@ -1,7 +1,7 @@
 <div class="dates">
 	<h3>Paper Submission Deadlines</h3>
 	
-	<p>Paper Submission Deadline: 3rd June, 2013</p>
-	<p>Notification of Acceptance: 24th June, 2013</p>
-	<p>Camera ready Deadline:  22nd July, 2013</p>
+	<p>Paper Submission Deadline: 3<sup>rd</sup> June, 2013</p>
+	<p>Notification of Acceptance: 24<sup>th</sup> June, 2013</p>
+	<p>Camera ready Deadline:  22<sup>nd</sup> July, 2013</p>
 </div>
