696f9de4e35399850e279d7a66d315e6ca3e9ea8
Albertini <albertini.da@gmail.com>
2013-05-29 13:13:38 +0200

    submission page

--- pages/submission.php
+++ pages/submission.php
@@ -4,6 +4,9 @@
 	<p>This workshop's objective is to create a forum where novel, visionary, or even groundbreaking ideas will be presented, discussed, and debated. To this direction, we encourage authors to submit vision, system, original research, practical experience or position papers that are not being considered for publication in any other forum. Papers submitted should be at most six (6) pages long. Papers should be submitted electronically as PDF files and be formatted using the VLDB format. Detailed formatting instructions and samples are available here:</p>
 
 	<a href="http://www.vldb.org/2013/formatting_guidelines.html"><h3>VLDB Guidelines</h3></a>
+	
+	<!-- https://www.easychair.org/conferences/?conf=persdb2013 -->
+	<p>The online submission site is <a href="https://www.easychair.org/conferences/?conf=persdb2013">Easychair</a></p>
 </div>
 
 <?php include('dates.php');?>
