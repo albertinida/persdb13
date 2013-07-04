c704d6ef01e600cfbcffdb04b684180795c8396f
Albertini <albertini.da@gmail.com>
2013-07-04 18:49:35 +0200

    date aggiornate

--- pages/dates.php
+++ pages/dates.php
@@ -1,8 +1,7 @@
 <div class="dates">
 	<h3>Paper Submission Deadlines</h3>
 	
-	<strike><p>Paper Submission Deadline: 3<sup>rd</sup> June, 2013</p></strike>
-	<p>Paper Submission Deadline: 10<sup>th</sup> June, 2013</p>
-	<p>Notification of Acceptance: 24<sup>th</sup> June, 2013</p>
-	<p>Camera ready Deadline:  22<sup>nd</sup> July, 2013</p>
+	<p>Paper Submission Deadline: <strike>3<sup>rd</sup> June, 2013</strike>&nbsp;10<sup>th</sup> June, 2013</p>
+	<p>Notification of Acceptance: <strike>24<sup>th</sup> June, 2013</strike>&nbsp;15<sup>th</sup> July, 2013</p>
+	<p>Camera ready Deadline:  <strike>22<sup>nd</sup> July, 2013</strike>&nbsp;25<sup>th</sup> July, 2013</p>
 </div>
