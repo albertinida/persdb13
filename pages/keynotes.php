9e90b7e1b6b2e2cee019059e1536b6776f7f6db0
Albertini <albertini.da@gmail.com>
2013-07-29 10:20:08 +0200

    pitoura keynote and program timetable

--- pages/keynotes.php
+++ pages/keynotes.php
@@ -8,4 +8,15 @@
 
 	</div>
 	
+	<div class="keynote">
+		<h3>Keynote speaker: Evaggelia Pitoura</h3>
+		
+		<h4>Two of the Many Faces of Ranking: Diversity and Time</h4>
+		
+		<p>What makes a query result interesting besides relevance? This talk focuses on two of the many ranking criteria: (a) diversity and (b) change through time. Diversity has recently attracted a lot of attention as a means of increasing user satisfaction in recommendation systems, information retrieval and database queries. Diversification takes many forms such as increasing novelty and covering different query aspects and user information needs. In our recent research, we have proposed efficient indexing techniques for supporting continuous diversification as well as a new definition of diversity based on graph theory that provides a novel perspective to the problem. Then, I will touch upon our new work on capturing and querying the history of evolving social graphs through time.</p>
+		
+		<p>Short bio:<br/>Evaggelia Pitoura is a Professor and Associate Chair of the Computer Science Department at the University of Ioannina, Greece where she also leads the Distributed Management of Data Laboratory.  She received a BSc degree from the University of Patras, Greece, and an MS and PhD degree from Purdue University, USA. Her research interests are in the area of distributed data management systems. Her publications in this area include more than 150 articles in international journals (including TODS, TKDE, TPDS, PVLDB) and conferences (including SIGMOD, ICDE, EDBT, CIKM, WWW) and a highly-cited book on mobile computing.  Her research in distributed computing and more recently in social and cloud computing has been funded by the EC and national sources. She is the recipient of two Best Paper Awards (ICDE 1999, DBSocial 2013), a Marie Currie Fellowship (2009) and two Recognition of Service Awards from ACM. She has served as a group leader, senior PC member and co-chair for many data management conferences (including SIGMOD, WWW, CIKM, EDBT, MDM, VLDB, WISE, ICDE).</p>
+		
+	</div>
+	
 </div>
--- pages/program.php
+++ pages/program.php
@@ -1,1 +1,38 @@
-<h3>TBA</h3>
+<div class="program">
+
+
+	<p>8.30 Welcome and registration</p>
+
+	<p>8.45 - 10.00 keynote<br/> 
+	Maurice Hendrix<br/>
+	Serious games meets Adaptive Hypermedia:<br/>
+	Integrating games into web-based e-learning systems</p>
+
+
+	<p>coffee break</p>
+
+	<p>10.30- 11.10<br/> 
+	 Peckalytics: Analyzing Experts and Interests on Twitter<br/>
+	 Alex Cheng, Nilesh Bansal, Nick Koudas</p>
+
+	<p>11.10 - 11.40<br/>
+	Rubi Boim and Tova Milo<br/>
+	Recommendation by Examples</p>
+
+	<p>Lunch</p>
+
+	<p>13.30 - 14.30 keynote<br/>
+	Evaggelia Pitoura<br/>
+	Two of the Many Faces of Ranking: Diversity and Time</p>
+
+
+	<p>14.30 - 15.30<br/> 
+	Social Search Queries in Time,<br/>
+	Georgia Koloniari and Kostas Stefanidis</p> 
+
+	<p>Coping with the Persistent Coldstart Problem,<br/>
+	Siarhei Bykau, Georgia Koutrika and Yannis Velegrakis</p>        
+
+	<p>15.30 conclusion</p>
+	
+</div>
